abb5c124f919741a6ed539eea022ebea9afe7858
pcy133 <39108097+pcy133@users.noreply.github.com>
2024-07-23 21:02:05 +0800

    Update app.py
    updated

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
+import os
 import telebot
+from flask import Flask, request
 
-# 替换为你的机器人 TOKEN
-TOKEN = '7163080666:AAFNETst95bbshxfhbNxTF0jMRvNpqruySE'
-
+TOKEN = os.environ.get('7163080666:AAFNETst95bbshxfhbNxTF0jMRvNpqruySE')
 bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
-print("Bot is running...")
-bot.polling()
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://cy1sryan-8e7ebb2bd5bf.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
